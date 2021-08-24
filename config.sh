8e1534cdef0e3471691d251dd81e67c2cdfa0b59
Andrew <x.and.i.rew.e@gmail.com>
2021-08-25 01:59:09 +1000

    updated nvim formatter config

---
+++ config.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+# need to install dependencies first
+echo `realpath $(dirname "$0")`
+
+# Create symbolic links
+# ln -s "$dotconfig/nvim ~/.config"
+
--- nvim/lua/plugins/formatter.lua
+++ nvim/lua/plugins/formatter.lua
@@ -6,52 +6,58 @@ return function()
 		filetype = {
 			-- general remove whitespace
 			javascript = {
-        -- prettier
-       function()
-          return {
-            exe = "prettier",
-            args = {"--stdin-filepath", vim.api.nvim_buf_get_name(0), '--single-quote'},
-            stdin = true
-          }
-        end
-    },
-		lua = {
-        -- luafmt
-        function()
-          return {
-            exe = "luafmt",
-            args = {"--indent-count", 2, "--stdin"},
-            stdin = true
-          }
-        end
-    },
-    cpp = {
-        -- clang-format
-       function()
-          return {
-            exe = "clang-format",
-            args = {"--assume-filename", vim.api.nvim_buf_get_name(0)},
-            stdin = true,
-            cwd = vim.fn.expand('%:p:h')  -- Run clang-format in cwd of the file.
-          }
-        end
-    },
-		python = {
-			-- black
-			function()
-				return {
-					exe = "black",
-					stdin = true,
-				}
-			end
-		}
+				-- prettier
+				function()
+					return {
+						exe = "prettier",
+						args = {"--stdin-filepath", vim.api.nvim_buf_get_name(0), '--single-quote'},
+						stdin = true
+					}
+				end
+			},
+			lua = {
+				-- stylua (TODO: get from AUR)
+				function()
+					return {
+						exe = "stylua",
+						args = {"--indent-count", 2},
+						stdin = true
+					}
+				end
+			},
+			cpp = {
+				-- clang-format
+				function()
+					return {
+						exe = "clang-format",
+						args = {"--assume-filename", vim.api.nvim_buf_get_name(0)},
+						stdin = true,
+						cwd = vim.fn.expand('%:p:h')  -- Run clang-format in cwd of the file.
+					}
+				end
+			},
+			python = {
+				-- black
+				function()
+					return {
+						exe = "black",
+						stdin = true,
+					}
+				end
+			}
 		}
 	}
 	-- Format on save
 	-- vim.api.nvim_exec([[
--- augroup FormatAutogroup
-  -- autocmd!
-  -- autocmd BufWritePost *.js,*.rs,*.lua FormatWrite
--- augroup END
--- ]], true)
+	-- augroup FormatAutogroup
+	-- autocmd!
+	-- autocmd BufWritePost *.js,*.rs,*.lua FormatWrite
+	-- augroup END
+	-- ]], true)
+	vim.api.nvim_exec([[
+	augroup FormatAutogroup
+	autocmd!
+	autocmd BufWritePre * lua vim.lsp.buf.formatting_sync(nil, 1000)
+	augroup END
+	]], true)
 end

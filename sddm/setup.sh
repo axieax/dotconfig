7c74cdacac33d9ee1ca9be7061b695c199f599b4
Andrew <x.and.i.rew.e@gmail.com>
2021-08-28 12:56:23 +1000

    old xmonad

---
+++ sddm/setup.sh
@@ -0,0 +1,5 @@
+# Preferred sddm theme: arcolinux-sugar-candy with arco-login.jpg background, middle form, lowercase
+
+sudo ln -s /usr/share/backgrounds/arcolinux/arco-login.jpg /usr/share/sddm/themes/arcolinux-sugar-candy/Backgrounds
+# theme.conf
+
--- setup.sh
+++ setup.sh
@@ -6,7 +6,4 @@ echo `realpath $(dirname "$0")`
 # Create symbolic links
 # ln -s "$dotconfig/nvim ~/.config"
 
-# Preferred sddm theme: arcolinux-sugar-candy with arco-login.jpg background, middle form
-# Issue: auto capitalisation
-
 

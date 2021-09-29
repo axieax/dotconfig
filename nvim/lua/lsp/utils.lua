ef9d2d7e2c0994d3d2d3ef7b0ae64d456d15e4f1
Andrew <x.and.i.rew.e@gmail.com>
2021-09-29 14:45:03 +1000

    updated jdtls config (before revert)

--- nvim/lua/lsp/utils.lua
+++ nvim/lua/lsp/utils.lua
@@ -1,3 +1,4 @@
+-- TODO: Java code actions from https://github.com/mfussenegger/nvim-jdtls#usage
 local M = {}
 
 local java_bundles = {
@@ -24,15 +25,51 @@ M.language_server_overrides = {
       -- jdtls start script
       vim.fn.expand("~/.local/share/nvim/lspinstall/java/jdtls.sh"),
       -- workspace data directory
-      -- vim.fn.expand("~/java/workspace/" .. vim.fn.fnamemodify(vim.fn.getcwd(), ":p:h:t")),
-      require("jdtls.setup").find_root({ ".git" }),
+      vim.fn.expand("~/java/workspaces/" .. vim.fn.fnamemodify(vim.fn.getcwd(), ":p:h:t")),
+      -- vim.fn.expand("~/java/workspaces/" .. require("jdtls.setup").find_root({ ".git" })),
+      -- require("jdtls.setup").find_root({ ".git" }),
     },
     on_attach = function(client, bufnr)
+      -- require("lsp").on_attach(client, bufnr)
       require("jdtls.setup").add_commands()
       require("jdtls").setup_dap({ hotcodereplace = "auto" })
       require("jdtls.dap").setup_dap_main_class_configs()
+      -- client.resolved_capabilities.document_formatting = false
+
+      -- Telescope
+      local finders = require("telescope.finders")
+      local sorters = require("telescope.sorters")
+      local actions = require("telescope.actions")
+      local pickers = require("telescope.pickers")
+      require("jdtls.ui").pick_one_async = function(items, prompt, label_fn, cb)
+        local opts = {}
+        pickers.new(opts, {
+          prompt_title = prompt,
+          finder = finders.new_table({
+            results = items,
+            entry_maker = function(entry)
+              return {
+                value = entry,
+                display = label_fn(entry),
+                ordinal = label_fn(entry),
+              }
+            end,
+          }),
+          sorter = sorters.get_generic_fuzzy_sorter(),
+          attach_mappings = function(prompt_bufnr)
+            actions.select_default:replace(function()
+              local selection = actions.get_selected_entry(prompt_bufnr)
+              actions.close(prompt_bufnr)
+
+              cb(selection.value)
+            end)
+
+            return true
+          end,
+        }):find()
+      end
     end,
-    root_dir = require("jdtls.setup").find_root({ "gradle.build", "pom.xml" }),
+    root_dir = require("jdtls.setup").find_root({ ".git", "gradle.build", "pom.xml", "gradlew" }),
   },
   -- haskell = {
   -- Modified from https://github.com/neovim/nvim-lspconfig/blob/master/lua/lspconfig/hls.lua
@@ -45,7 +82,6 @@ M.language_server_overrides = {
 }
 
 function M.jdtls_setup()
-  print(vim.inspect(M.language_server_overrides.java))
   require("jdtls").start_or_attach(M.language_server_overrides.java)
 end
 

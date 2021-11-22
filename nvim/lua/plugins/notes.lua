45cdb50e8173a12641246aa631301c020a8e7632
Andrew <x.and.i.rew.e@gmail.com>
2021-11-23 04:50:02 +1100

    added extra todo comments, switched spelling binding

--- nvim/lua/plugins/notes.lua
+++ nvim/lua/plugins/notes.lua
@@ -31,11 +31,28 @@ return function()
         color = "info",
         alt = { "INFO" },
       },
+      QUESTION = {
+        icon = " ",
+        color = "error",
+        alt = { "IDK" },
+      },
+      BOOKMARK = {
+        icon = " ",
+        color = "warning",
+        alt = { "CHECK", "READ", "LOOK", "REVIEW", "HERE" },
+      },
     },
     highlight = {
       before = "empty",
       keyword = "bg",
       after = "fg",
+    },
+    search = {
+      -- NOTE: doesn't seem to work
+      -- regex that will be used to match keywords.
+      -- don't replace the (KEYWORDS) placeholder
+      -- pattern = [[\b(KEYWORDS):]], -- ripgrep regex
+      pattern = [[\b(KEYWORDS)\b]], -- match without the extra colon. You'll likely get false positives
     },
   })
 end

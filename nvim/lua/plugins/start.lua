8a209897c8abf82f610b311ba99aa86dfa6784a6
Andrew <x.and.i.rew.e@gmail.com>
2021-12-16 03:30:28 +1100

    updated dashboard

--- nvim/lua/plugins/start.lua
+++ nvim/lua/plugins/start.lua
@@ -1,4 +1,6 @@
 -- https://github.com/goolord/alpha-nvim --
+-- TODO: customise like https://github.com/goolord/alpha-nvim/discussions/16#discussioncomment-1308930
+-- narrower center align
 
 return function()
   local dashboard = require("alpha.themes.dashboard")
@@ -15,19 +17,21 @@ return function()
   }
 
   dashboard.section.buttons.val = {
-    dashboard.button("n", "New File", "<CMD>ene <BAR> startinsert<CR>"),
-    dashboard.button("f", "Find Files", "<CMD>lua require'plugins.telescope'.file_search()<CR>"),
-    dashboard.button("g", "Live Grep", "<CMD>Telescope live_grep<CR>"),
-    dashboard.button("b", "Bookmarks", "<CMD>Telescope marks<CR>"),
-    dashboard.button("o", "Recent Files", "<CMD>Telescope oldfiles<CR>"), -- TODO: use frecency
-    dashboard.button("p", "Find Projects", "<CMD>Telescope projects<CR>"),
-    dashboard.button("s", "Restore Session", "<CMD>LoadLastSession<CR>"),
-    dashboard.button("S", "Find Sessions", "<CMD>Telescope sessions<CR>"),
-    dashboard.button("c", "Neovim Config", "<CMD>e ~/.config/nvim/lua/plugins/init.lua<CR>"),
-    dashboard.button("q", "Quit Neovim", "<CMD>qa<CR>"),
+    dashboard.button("n", "  New File", "<CMD>ene <BAR> startinsert<CR>"),
+    dashboard.button("f", "  Find Files", "<CMD>lua require'plugins.telescope'.file_search()<CR>"),
+    dashboard.button("g", "  Live Grep", "<CMD>Telescope live_grep<CR>"),
+    dashboard.button("b", "  Bookmarks", "<CMD>Telescope marks<CR>"),
+    -- TODO: orgmode notes
+    dashboard.button("o", "  Recent Files", "<CMD>Telescope oldfiles<CR>"), -- TODO: use frecency
+    dashboard.button("p", "  Find Projects", "<CMD>Telescope projects<CR>"),
+    dashboard.button("s", "  Restore Session", "<CMD>LoadLastSession<CR>"),
+    dashboard.button("S", "  Find Sessions", "<CMD>Telescope sessions<CR>"),
+    dashboard.button("c", "﫸 Neovim Config", "<CMD>e ~/.config/nvim/lua/plugins/init.lua<CR>"),
+    dashboard.button("u", "  Update Plugins", "<CMD>PackerSync<CR>"),
+    dashboard.button("q", "⏻  Quit Neovim", "<CMD>qa<CR>"),
   }
 
-  dashboard.section.footer.val = " https://github.com/axieax/"
+  dashboard.section.footer.val = "  https://github.com/axieax/"
 
   require("alpha").setup(dashboard.opts)
 end

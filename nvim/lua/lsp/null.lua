a25b1bed89f6ebe7086d141fe79ae75b03de9b59
Andrew <x.and.i.rew.e@gmail.com>
2022-02-03 16:27:28 +1100

    updated goimports for Canva CI

--- nvim/lua/lsp/null.lua
+++ nvim/lua/lsp/null.lua
@@ -30,7 +30,11 @@ function M.formatting_sources()
     -- PYTHON: pip install isort (import sort)
     formatting.isort,
     -- GOLANG: go install golang.org/x/tools/cmd/goimports@latest
-    formatting.goimports,
+    -- NOTE: this superset of gomft is preferred if installed
+    formatting.goimports.with({
+      -- TEMP: group local Canva packages
+      extra_args = { "-local", "github.com/canva" },
+    }),
     -- HASKELL: yay -S brittany
     formatting.brittany,
     -- C*: yarn global add clang_format

7aeb937df4b4d2de838adf01f2cba1290bb51549
Andrew <x.and.i.rew.e@gmail.com>
2022-05-23 17:55:22 +1000

    added zscroll to polybar setup script

--- polybar/setup.sh
+++ polybar/setup.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
 source "$HOME/dotconfig/setup-utilities.sh"
 
+action="install zscroll"
+if is_linux && ! check_dependency zscroll && confirm "$action"; then
+  yay -S zscroll
+fi
+
 link_config "$HOME/dotconfig/polybar" "$HOME/.config/polybar"

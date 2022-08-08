4ae885c280beaceb6888d92d065bd83dd4c2b6b3
Andrew <x.and.i.rew.e@gmail.com>
2022-08-08 12:25:51 +1000

    updated incline config

--- nvim/lua/axie/plugins/incline.lua
+++ nvim/lua/axie/plugins/incline.lua
@@ -1,26 +1,38 @@
 -- https://github.com/b0o/incline.nvim --
 
 return function()
-  local maroon = "#EBA0AC"
-  local mauve = "#CBA6F7"
+  local colours = {
+    maroon = "#EBA0AC",
+    mauve = "#CBA6F7",
+    yellow = "#F9E2AF",
+  }
+ 
   require("incline").setup({
     highlight = {
       groups = {
         InclineNormal = {
-          guifg = maroon,
+          guifg = colours.maroon,
+          gui = "bold",
         },
         InclineNormalNC = {
-          guifg = mauve,
+          guifg = colours.mauve,
+        },
+        InclineModified = {
+          guifg = colours.yellow,
         },
       },
     },
     render = function(props)
-      local filename = vim.fn.fnamemodify(vim.api.nvim_buf_get_name(props.buf), ":t")
+      local bufname = vim.api.nvim_buf_get_name(props.buf)
+      local filename = bufname ~= "" and vim.fn.fnamemodify(bufname, ":t") or "[No Name]"
       local icon, color = require("nvim-web-devicons").get_icon_color(filename)
+      local modified = vim.api.nvim_buf_get_option(props.buf, "modified") and " *" or " "
+
       return {
         { icon, guifg = color },
         { " " },
         { filename },
+        { modified, group = "InclineModified" },
       }
     end,
   })

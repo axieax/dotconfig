47b856564d5e4e470a69590da8788c8bae68c1ed
Andrew <x.and.i.rew.e@gmail.com>
2022-08-20 01:37:00 +1000

    feat(packer): add ability to specify additional options for use wrapper

--- nvim/lua/axie/plugins/init.lua
+++ nvim/lua/axie/plugins/init.lua
@@ -34,9 +34,7 @@ return packer.startup({
       use({
         "max397574/better-escape.nvim",
         config = function()
-          require("better_escape").setup({
-            mapping = { "jk", "kj" },
-          })
+          require("better_escape").setup({ mapping = { "jk", "kj" } })
         end,
       })
     end
